cbed30d71ec8139c2533a6c805748e6f62b7002c
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-18 14:19:13 -0500

    not sure if there are spaces between the name & the lastname

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
+    file = open('./src/encoded.txt', 'r', encoding='utf-8')
+    text = file.read()
+    secret = ''.join(re.findall(r"[a-z\s]+",text))
+    print(secret)
+    
 
 
 if __name__ == '__main__':
